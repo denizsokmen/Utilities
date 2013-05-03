503448127c16b17fcc72a6329c4943e296d33ed2
Deniz <xantier277@gmail.com>
2013-05-03 19:01:27 +0300

    Network Byte Order

---
+++ NetworkTypes.h
@@ -0,0 +1,36 @@
+#ifdef WIN32
+#include <WinSock.h>
+#pragma comment(lib, "Ws2_32.lib")
+#else
+#include <netinet/in.h>
+#endif
+
+#include <stdint.h>
+
+
+struct NET32 {
+	int32_t  value;
+	operator int () const
+	{
+		return( ntohl(value) );
+	}
+	NET32 & operator = ( int32_t val )
+	{
+		value = htonl( val );
+		return( * this );
+	}
+};
+
+struct NET16 {
+	int16_t  value;
+	operator int () const
+	{
+		return( ntohs(value) );
+	}
+	NET16 & operator = ( int16_t val )
+	{
+		value = htons( val );
+		return( * this );
+	}
+};
+
